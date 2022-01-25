c067941e7be4ae53d1b1a5265031b41e649a0fdf
Joe Swann <joe@1of1studio.com>
2022-01-25 15:17:29 +1300

    Add @sanity/base/components to module-resolver no-op

--- sanity-codegen.config.ts
+++ sanity-codegen.config.ts
@@ -1,14 +1,36 @@
 import { SanityCodegenConfig } from "sanity-codegen";
+import { defaultBabelOptions } from "sanity-codegen/cli";
+
+const babelOptions = {
+  ...defaultBabelOptions,
+  plugins: [
+    ...defaultBabelOptions.plugins.filter(([key]) => key !== "module-resolver"),
+    [
+      "module-resolver",
+      {
+        root: ["."],
+        alias: {
+          "part:@sanity/base/schema-creator":
+            "sanity-codegen/schema-creator-shim",
+          "all:part:@sanity/base/schema-type":
+            "sanity-codegen/schema-type-shim",
+          "^part:.*": "sanity-codegen/no-op",
+          "part:@sanity/base/schema-type": "sanity-codegen/schema-type-shim",
+          "^config:.*": "sanity-codegen/no-op",
+          "^all:part:.*": "sanity-codegen/no-op",
+          "^part:@sanity/base/*": "sanity-codegen/no-op",
+          "@sanity/base/components": "sanity-codegen/no-op",
+          // '<forked-package-name>': 'sanity-codegen/no-op',
+        },
+      },
+    ],
+  ],
+};
 
 const config: SanityCodegenConfig = {
   schemaPath: "./schemas/schema.js",
   outputPath: "./schema.ts",
-
-  // NOTE: The CLI ships with a pre-configured babel config that shims out
-  // the Sanity parts system. This babel config does not read from any
-  // `.babelrc` or `babel.config.js`. You can only configure extra babel
-  // options here.
-  // babelOptions: require('./.babelrc.json'), // (optional)
+  babelOptions,
 };
 
 export default config;
